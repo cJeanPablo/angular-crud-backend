f5cf8d9101c564eff107bfe126aeadd39d2847a0
cJeanPablo <cjeanpablo@gmail.com>
2022-02-11 15:35:17 -0300

    added product-create component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,18 +1,26 @@
-import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { HomeComponent } from './views/home/home.component';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ProductCreateComponent } from "./components/product/product-create/product-create.component";
+import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
+import { HomeComponent } from "./views/home/home.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [{
-  path: "", 
-  component: HomeComponent
-},{
-  path: "products",
-  component: ProductCrudComponent
-}];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+  },
+  {
+    path: "products",
+    component: ProductCrudComponent,
+  },
+  {
+    path: "products/create",
+    component: ProductCreateComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
