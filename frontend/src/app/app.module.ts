3fae4d60ed75198fe9db337f8133ea1db7b30752
cJeanPablo <cjeanpablo@gmail.com>
2022-02-13 11:37:46 -0300

    added product.service.ts and buttons in product-create

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -17,7 +17,9 @@ import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-import { MatButtonModule } from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 
 
 
@@ -39,7 +41,8 @@ import { MatButtonModule } from '@angular/material/button'
     MatSidenavModule,
     MatListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
